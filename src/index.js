47d6b2ec2c3a94f909cfd1701dfe056520e67fd5
Roylu7384 <roylu7384@gmail.com>
2022-06-23 16:26:38 -0400

    Refactor index.js to use prisma client connecting to sqlite db

--- src/index.js
+++ src/index.js
@@ -1,80 +1,66 @@
 const { ApolloServer } = require("apollo-server");
-const fs = require('fs');
-const path = require('path');
+const { PrismaClient } = require("@prisma/client");
 
-// in memory mock data
-let links = [
-  {
-    id: "link-0",
-    url: "www.hotwographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
+const fs = require("fs");
+const path = require("path");
 
 // resolver
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => {
-      let link = null;
-
-      links.forEach(l => {
-        if (l.id === args.id) link = l;
-      });
-     
-      return link;
+    feed: (parent, args, context) => {
+      return context.prisma.link.findMany();
     }
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent, args) => {
-      let linkIndex;
-
-      links.forEach((l, index) => {
-        if (l.id === args.id) linkIndex = index;
-      });
-
-      links[linkIndex].url = args.url;
-      links[linkIndex].description = args.description;
-
-      return links[linkIndex];
-    },
-    deleteLink: (parent, args) => {
-      let newLinks = [];
-      let linkToBeDelete;
-
-      links.forEach(link => {
-        if (link.id === args.id) {
-          linkToBeDelete = link;
-          return;
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description
         }
-        newLinks.push(link);
       });
+      return newLink;
+    },
+    // updateLink: (parent, args) => {
+    //   let linkIndex;
 
-      links = newLinks;
-      return linkToBeDelete;
-    }
+    //   links.forEach((l, index) => {
+    //     if (l.id === args.id) linkIndex = index;
+    //   });
+
+    //   links[linkIndex].url = args.url;
+    //   links[linkIndex].description = args.description;
+
+    //   return links[linkIndex];
+    // },
+    // deleteLink: (parent, args) => {
+    //   let newLinks = [];
+    //   let linkToBeDelete;
+
+    //   links.forEach(link => {
+    //     if (link.id === args.id) {
+    //       linkToBeDelete = link;
+    //       return;
+    //     }
+    //     newLinks.push(link);
+    //   });
+
+    //   links = newLinks;
+    //   return linkToBeDelete;
+    // }
   }
 };
 
 // config apollo server
+const prisma = new PrismaClient();
+
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, 'schema.graphql'),
-    'utf8'
-  ),
-  resolvers
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+  context: {
+    prisma
+  }
 });
 
 // start the server
