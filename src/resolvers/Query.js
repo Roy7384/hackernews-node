b8bacd043f8477d7b6c83dc12914d03e09d8cb2f
Roylu7384 <roylu7384@gmail.com>
2022-07-04 12:22:24 -0400

    Add info resolver back to root query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,11 @@ function feed(parent, args, context) {
   return context.prisma.link.findMany();
 }
 
+function info() {
+  return "Hello, this is the graphql server for a hackernews clone";
+}
+
 module.exports = {
   feed,
-}
+  info
+};
