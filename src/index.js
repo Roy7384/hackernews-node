49baf95c5026e04cbc4a2090190217c643f0e9f7
Roylu7384 <roylu7384@gmail.com>
2022-07-04 15:38:06 -0400

    Import Subscription resolver and add to server

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
 
 // resolver
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link
 };
